952de225ff959bbc7eb7feed750a2deda83868ea
Fernando Marques Lisboa <fernando.lisboa@viprede.com>
2022-03-13 17:53:17 -0300

    adding deposit function

--- src/index.js
+++ src/index.js
@@ -20,11 +20,16 @@ app.use(express.json());
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
-  const id = uuidv4();
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" });
+  }
   customers.push({
     cpf,
     name,
-    id,
+    id: uuidv4(),
     statement: [],
   });
 
@@ -36,6 +41,20 @@ app.post("/account", (request, response) => {
 app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
   const { customer } = request;
   return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'credit'
+  }
+  customer.statement.push(statementOperation);
+  
+  return response.status(201).send();
 });
 
 //localhost:3333
