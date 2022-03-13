1d34356743e99fe0a749cb2724b697de5655f203
Fernando Marques Lisboa <fernando.lisboa@viprede.com>
2022-03-13 17:38:36 -0300

    adding middleware to verify if account exists with cpf

--- src/index.js
+++ src/index.js
@@ -1,32 +1,42 @@
-const express = require('express');
-const { v4: uuidv4 } = require('uuid');
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 const customers = [];
 
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+  const customer = customers.find(customer => customer.cpf === cpf);
+  if(!customer) {
+    return response.status(400).json({ error: 'Customer not found!' })
+  }
+  request.customer = customer;
+  return next();
+}
+
 app.use(express.json());
 
-/** user data:
- * cpf - string
- * name - string
- * id - uuid
- * statement - []
- */
-
 app.post("/account", (request, response) => {
-    const { cpf, name } = request.body;
-    const id = uuidv4();
-    customers.push({
-        cpf,
-        name,
-        id,
-        statement: []
-    });
+  const { cpf, name } = request.body;
+  const id = uuidv4();
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: [],
+  });
 
-    return response.status(201).send();
+  return response.status(201).send();
 });
 
+// app.use(verifyIfExistsAccountCPF);
+
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  return response.json(customer.statement);
+});
 
 //localhost:3333
 app.listen(3333);
